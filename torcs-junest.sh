eca3808cfda1c877cf28b92f4985f04b34f18f95
iVAN <88724353+ivan-hc@users.noreply.github.com>
2025-11-20 19:59:39 +0100

    Update torcs-junest.sh

--- torcs-junest.sh
+++ torcs-junest.sh
@@ -71,9 +71,6 @@ _enable_multilib() { MULTILIB_ON="1"; }
 #	INSTALL PROGRAMS USING YAY
 ##########################################################################################################################################################
 
-_JUNEST_CMD -- yay -Syy
-_JUNEST_CMD -- gpg --keyserver keyserver.ubuntu.com --recv-key C01E1CAD5EA2C4F0B8E3571504C367C218ADD4FF # UNCOMMENT IF YOU USE THE AUR
-
 [ -f ../archimage-builder.sh ] && source ../archimage-builder.sh install "$@"
 
 _JUNEST_CMD -- yay --noconfirm -S  --mflags --skipinteg "$APP"
